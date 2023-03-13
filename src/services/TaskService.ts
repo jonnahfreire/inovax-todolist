e45f59f5a22b05afff5733560afb31f472cc9801
jonnahfreire <jonnas.freire17@gmail.com>
2023-03-13 17:21:51 -0300

    Update: tasks returns

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,11 +1,53 @@
 import { TaskProps } from "../contexts/TaskContext";
 
-export async function sCreateTask(text: string): Promise<void> {
-    
+const API = 'http://localhost:3000'
+
+export async function getTasks(): Promise<TaskProps[]> {
+    const response = await fetch(`${API}/tasks`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+
+    return await response.json();
 }
 
-export async function sEditTask(task: TaskProps): Promise<void> {}
+export async function sCreateTask(task: TaskProps): Promise<void> {
+    const response = await fetch(`${API}/task`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: task.text, done: task.done })
+    })
 
-export async function sToggleTaskDone(task: TaskProps): Promise<void> {}
+    return await response.json();
+}
 
-export async function sDeleteTask(id: string): Promise<void> {}
+export async function sEditTask(task: TaskProps): Promise<void> { 
+    const response = await fetch(`${API}/task/${task.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text: task.text, done: task.done })
+    })
+
+    return await response.json();
+}
+
+export async function sToggleTaskDone(task: TaskProps): Promise<void> {
+    return await sEditTask(task);
+}
+
+export async function sDeleteTask(id: string): Promise<void> { 
+    const response = await fetch(`${API}/task/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+
+    return await response.json();
+}
